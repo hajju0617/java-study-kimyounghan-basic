7d8e8627dc50d22d7d6ad4eea018e7d29f491866
hajju0617 <joowon023617@gmail.com>
2024-12-11 23:59:00 +0900

    java - 접근 제어자

---
+++ src/main/java/org/example/access/b/AccessOuterMain.java
@@ -0,0 +1,24 @@
+package org.example.access.b;
+
+import org.example.access.a.AccessData;
+
+public class AccessOuterMain {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        // public 호출 가능.
+        data.publicField = 1;
+        data.publicMethod();
+
+        // 다른 패키지 default 호출 불가.
+//        data.defaultField = 2;
+//        data.defaultMethod();
+
+        // private 호출 불가.
+//        data.privateField = 3;
+//        data.privateMethod();
+
+        // innerAccess()는 public 이므로 외부에서 호출가능.
+        data.innerAccess();
+    }
+}
